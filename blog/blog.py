6b669e3293299b850b04f0242e6c8dec1d6f76b8
brickatyourfeet <brickatyourfeet@gmail.com>
2018-01-09 17:53:27 -0800

    add working config file to run all tests

--- blog/blog.py
+++ blog/blog.py
@@ -1,3 +1,6 @@
+from post import Post
+
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -11,4 +14,7 @@ class Blog:
                                              's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
+        self.posts.append(Post(title, content))
+
+    def json(self):
         pass
